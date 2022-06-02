b49913c6de2e281800b8e7fb3398a8264b61ff13
TheNonal <81806620+TheNonal@users.noreply.github.com>
2022-06-02 13:18:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var placemarks = [
 				<img class="map_img" src="img/ko.jfif"/>
 
 				<div class="balloon_info">
-					<p href="">Пропала кошка, рыжий цвет. Пропала кошка, рыжий цвет. Пропала кошка, рыжий цвет. Пропала кошка, рыжий цвет. Пропала кошка, рыжий цвет.</p>
+					<p href="">Пропала кошка, рыжий цвет.</p>
 				</div>
 
 				<div class="balloon_contacts">
@@ -79,3 +79,25 @@ function init() {
     map.geoObjects.add(clusterer);
     clusterer.add(geoObjects);
 }
+
+$(function() {
+    document.getElementById('ajax-contact-form').addEventListener('submit', function(evt){
+      var http = new XMLHttpRequest(), f = this;
+      var th = $(this);
+      evt.preventDefault();
+      http.open("POST", "contact.php", true);
+      http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+          alert(http.responseText);
+          if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+            th.trigger("reset");
+          }
+        }
+      }
+      http.onerror = function() {
+        alert('Ошибка, попробуйте еще раз');
+      }
+      http.send(new FormData(f));
+    }, false);
+   
+  });
