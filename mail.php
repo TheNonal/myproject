249276696e9b74001a88dba2d55d4b123cea06dd
TheNonal <81806620+TheNonal@users.noreply.github.com>
2022-06-02 13:33:56 +0500

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,46 +1,41 @@
-<?php
-if (isset ($_POST['contactFF'])) {
-  $to = "oskonbaev.2k4@yandex.ru";
-  $from = "oskonbaev.2k4@yandex.ru";
-  $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
-  $message = "Имя пользователя: ".$_POST['nameFF']."\nEmail пользователя ".$_POST['contactFF']."\nТелефон пользователя ".$_POST['telFF']."\nСообщение: ".$_POST['projectFF']."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
- 
-  $boundary = md5(date('r', time()));
-  $filesize = '';
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: " . $from . "\r\n";
-  $headers .= "Reply-To: " . $from . "\r\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-  $message="
-Content-Type: multipart/mixed; boundary=\"$boundary\"
- 
---$boundary
-Content-Type: text/plain; charset=\"utf-8\"
-Content-Transfer-Encoding: 7bit
- 
-$message";
-     if(is_uploaded_file($_FILES['fileFF']['tmp_name'])) {
-         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF']['tmp_name'])));
-         $filename = $_FILES['fileFF']['name'];
-         $filetype = $_FILES['fileFF']['type'];
-         $filesize = $_FILES['fileFF']['size'];
-         $message.="
- 
---$boundary
-Content-Type: \"$filetype\"; name=\"$filename\"
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment; filename=\"$filename\"
- 
-$attachment";
-     }
-   $message.="
---$boundary--";
- 
-  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
-    mail($to, $subject, $message, $headers);
-    echo $_POST['nameFF'].', Ваше сообщение отправлено, спасибо!';
-  } else {
-    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
-  }
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$password = $_POST['user_password'];
+$info = $_POST['user_info'];
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = $email; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = $password; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom($email); // от кого будет уходить письмо?
+$mail->addAddress('oskonbaev.2k4@yandex.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка';
+$mail->Body    = '' .$name. ' оставил объявление ' .$info. ', его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
 }
 ?>
